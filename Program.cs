f89bd3725d602f689ceb07680e150876157613a5
rinat <rinat@mail.ru>
2022-02-09 00:31:19 +0300

    Main procedures

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DivAddMining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
